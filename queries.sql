bdcbb3bab75c4795780661103e8dc32f72fe5fbc
Bao Ngo <Owner@macbook-air.attlocal.net>
2021-11-19 17:58:49 -0600

    Ass 4

--- queries.sql
+++ queries.sql
@@ -1,19 +1,18 @@
 -- Part 1: Test it with SQL
-id int PK
-employer varchar(255)
-name varchar(255)
-skills varchar(255)
+SELECT COLUMN_NAME, DATA_TYPE
+FROM   INFORMATION_SCHEMA.COLUMNS
+WHERE  TABLE_NAME = 'job' AND TABLE_SCHEMA = 'techjobs';
 
 -- Part 2: Test it with SQL
 SELECT name
 FROM employer
-WHERE location = "Saint Louis City";
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 DROP TABLE jobs;
 
 -- Part 4: Test it with SQL
-SELECT DISTINCT name, description
+SELECT skill.name, skill.description
 FROM skill
 INNER JOIN job_skills ON job_skills.skills_id = skill.id
 ORDER BY name;
